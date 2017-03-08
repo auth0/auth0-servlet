06653db7683b6850af98e5171268c951cc1b9f4d
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-03-08 15:38:55 -0300

    remove public key certificate parser

--- src/main/java/com/auth0/lib/RequestProcessorFactory.java
+++ src/main/java/com/auth0/lib/RequestProcessorFactory.java
@@ -2,26 +2,10 @@ package com.auth0.lib;
 
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.jwk.JwkProvider;
-import org.apache.commons.lang3.Validate;
-import org.bouncycastle.util.io.pem.PemReader;
 
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Paths;
-import java.security.KeyFactory;
-import java.security.PublicKey;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Scanner;
 
 class RequestProcessorFactory {
-
-    private static final String RSA_CERTIFICATE_HEADER = "-----BEGIN CERTIFICATE-----";
 
     RequestProcessor forCodeGrant(AuthAPI client) {
         return new RequestProcessor(client);
@@ -37,45 +21,4 @@ class RequestProcessorFactory {
         return new RequestProcessor(client, verifier);
     }
 
-
-    /**
-     * Read the bytes of a PKCS8 RSA Public Key or Certificate.
-     *
-     * @param path the RSA certificate or public key file path.
-     * @return the parsed RSA Public Key.
-     * @throws IOException if something happened when trying to parse the certificate bytes.
-     */
-    //Visible for testing
-    @SuppressWarnings("WeakerAccess")
-    static RSAPublicKey readPublicKeyFromFile(final String path) throws IOException {
-        Validate.notNull(path);
-        Scanner scanner = null;
-        PemReader pemReader = null;
-        try {
-            scanner = new Scanner(Paths.get(path));
-            if (scanner.hasNextLine() && scanner.nextLine().startsWith(RSA_CERTIFICATE_HEADER)) {
-                FileInputStream fs = new FileInputStream(path);
-                CertificateFactory fact = CertificateFactory.getInstance("X.509");
-                X509Certificate cer = (X509Certificate) fact.generateCertificate(fs);
-                PublicKey key = cer.getPublicKey();
-                fs.close();
-                return (RSAPublicKey) key;
-            } else {
-                pemReader = new PemReader(new FileReader(path));
-                byte[] keyBytes = pemReader.readPemObject().getContent();
-                KeyFactory kf = KeyFactory.getInstance("RSA");
-                EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-                return (RSAPublicKey) kf.generatePublic(keySpec);
-            }
-        } catch (Exception e) {
-            throw new IOException("Couldn't parse the RSA Public Key / Certificate file.", e);
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
-            if (pemReader != null) {
-                pemReader.close();
-            }
-        }
-    }
 }
--- src/test/java/com.auth0/lib/RequestProcessorFactoryTest.java
+++ src/test/java/com.auth0/lib/RequestProcessorFactoryTest.java
@@ -9,17 +9,11 @@ import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.io.IOException;
-import java.security.interfaces.RSAPublicKey;
-
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
 public class RequestProcessorFactoryTest {
-
-    private static final String RS_CERTIFICATE = "src/test/resources/certificate.pem";
-    private static final String RS_PUBLIC_KEY = "src/test/resources/public.pem";
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
@@ -57,31 +51,6 @@ public class RequestProcessorFactoryTest {
         assertThat(processor, is(notNullValue()));
         assertThat(processor.client, is(client));
         assertThat(processor.verifier, is(notNullValue()));
-    }
-
-    @Test
-    public void shouldThrowOnReadRSAKeyWithNullPath() throws Exception {
-        exception.expect(NullPointerException.class);
-        RequestProcessorFactory.readPublicKeyFromFile(null);
-    }
-
-    @Test
-    public void shouldThrowOnReadRSAKeyFromMissingFile() throws Exception {
-        exception.expect(IOException.class);
-        exception.expectMessage("Couldn't parse the RSA Public Key / Certificate file.");
-        RequestProcessorFactory.readPublicKeyFromFile("/not/existing/file");
-    }
-
-    @Test
-    public void shouldReadRSAKeyFromPublicKeyFile() throws Exception {
-        RSAPublicKey key = RequestProcessorFactory.readPublicKeyFromFile(RS_PUBLIC_KEY);
-        assertThat(key, is(notNullValue()));
-    }
-
-    @Test
-    public void shouldReadRSAKeyFromCertificateFile() throws Exception {
-        RSAPublicKey key = RequestProcessorFactory.readPublicKeyFromFile(RS_CERTIFICATE);
-        assertThat(key, is(notNullValue()));
     }
 
 }
